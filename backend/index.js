dd59b52e3b182881370f675e4f1f6c4a01814ec5
GameoCoder <nitinpatna804421@gmail.com>
2025-04-10 17:04:49 +0530

    First commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.get('/', (req,res) => {
+	res.send('Student Login System Backend is running.');
+});
